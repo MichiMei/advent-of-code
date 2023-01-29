612339685f9354ac24c7b7fe6c1f939df844ecc0
Hans Michel Meißner <meissnha@informatik.hu-berlin.de>
2023-01-29 02:24:15 +0100

    added day 06

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -193,13 +193,13 @@ fn run_2016() -> io::Result<()> {
     let input = read_lines_untrimmed_from_file(&input_name)?;
     println!("y{}-d05_p1: {}", year, year_2015::day_05::part_1(&input).unwrap());
     println!("y{}-d05_p2: {}", year, year_2015::day_05::part_2(&input).unwrap());
-    /*
+
     // day06
     let input_name = input_folder.to_string() + "/input_day_06.txt";
     let input = read_lines_untrimmed_from_file(&input_name)?;
     println!("y{}-d06_p1: {}", year, year_2015::day_06::part_1(&input).unwrap());
     println!("y{}-d06_p2: {}", year, year_2015::day_06::part_2(&input).unwrap());
-
+    /*
     // day07
     let input_name = input_folder.to_string() + "/input_day_07.txt";
     let input = read_lines_untrimmed_from_file(&input_name)?;
---
+++ src/year_2016/day_06.rs
@@ -0,0 +1,210 @@
+use crate::errors::AoCError;
+
+pub fn part_1(input: &Vec<String>) -> Result<String, AoCError<String>> {
+    if input.len() < 1 {
+        return Err(AoCError::UnexpectedInputLength(
+            format!("Input needs to have at least one line.")
+        ))
+    }
+    let mut iter = input.iter();
+    let first = iter.next().expect("Is possible as length >= 1");
+    let mut error_corrector = ErrorCorrector::from_line(first)?;
+    for line in iter {
+        error_corrector.add_line(line)?;
+    }
+    if let Some(code) = error_corrector.get_most_frequent_code() {
+        Ok(code)
+    } else {
+        Err(AoCError::NoSolutionFoundError(
+            format!("No solution was found. At least one char is not unique.")
+        ))
+    }
+}
+
+pub fn part_2(input: &Vec<String>) -> Result<String, AoCError<String>> {
+    if input.len() < 1 {
+        return Err(AoCError::UnexpectedInputLength(
+            format!("Input needs to have at least one line.")
+        ))
+    }
+    let mut iter = input.iter();
+    let first = iter.next().expect("Is possible as length >= 1");
+    let mut error_corrector = ErrorCorrector::from_line(first)?;
+    for line in iter {
+        error_corrector.add_line(line)?;
+    }
+    if let Some(code) = error_corrector.get_least_frequent_code() {
+        Ok(code)
+    } else {
+        Err(AoCError::NoSolutionFoundError(
+            format!("No solution was found. At least one char is not unique.")
+        ))
+    }
+}
+
+struct ErrorCorrector {
+    chars: Vec<CharCounter>,
+}
+
+impl ErrorCorrector {
+    fn from_line(line: &str) -> Result<Self, AoCError<String>> {
+        let length = line.len();
+        let mut new = Self::new(length);
+        new.add_line(line)?;
+        Ok(new)
+    }
+
+    fn new(length: usize) -> Self {
+        let chars = vec![Default::default(); length];
+        Self{chars}
+    }
+
+    fn add_line(&mut self, line: &str) -> Result<(), AoCError<String>> {
+        if self.chars.len() != line.len() {
+            return Err(AoCError::BadInputFormat(
+                format!("A input line has the wrong size ({}), expected {}. \
+                    All lines must have equal length.", line.len(), self.chars.len())
+            ))
+        }
+        for (index, char) in line.chars().enumerate() {
+            self.chars[index].add_char(char)?;
+        }
+        Ok(())
+    }
+
+    fn get_most_frequent_code(&self) -> Option<String> {
+        self.chars.iter().map(|c| c.get_most_frequent()).collect()
+    }
+
+    fn get_least_frequent_code(&self) -> Option<String> {
+        self.chars.iter().map(|c| c.get_least_frequent()).collect()
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+struct CharCounter {
+    counts: [usize; 26],
+    max_count: usize,
+    max_char: Option<char>,
+}
+
+impl CharCounter {
+    fn add_char(&mut self, char: char) -> Result<(), AoCError<String>> {
+        let index = Self::char_to_usize(char)
+            .ok_or(AoCError::BadInputFormat(
+                format!("Unexpected character '{}'. Only a-z allowed.", char)
+            ))?;
+        assert!(index < self.counts.len());
+        let new_count = self.counts[index] + 1;
+        self.counts[index] = new_count;
+        if new_count == self.max_count {
+            self.max_char = None;
+        }
+        if new_count > self.max_count {
+            self.max_count = new_count;
+            self.max_char = Some(char);
+        }
+        Ok(())
+    }
+
+    fn get_most_frequent(&self) -> Option<char> {
+        self.max_char
+    }
+
+    fn get_least_frequent(&self) -> Option<char> {
+        let mut min_count = None;
+        let mut min_index = None;
+        for (index, count) in self.counts.iter().enumerate() {
+            if *count == 0 {
+                continue
+            }
+            if min_count.is_none() || count < min_count.unwrap() {
+                min_count = Some(count);
+                min_index = Some(index);
+            } else if count == min_count.unwrap() {
+                min_index = None;
+            }
+        }
+        let x = min_index.map(|t| Self::usize_to_char(t)).flatten();
+        x
+    }
+
+    fn char_to_usize(char: char) -> Option<usize> {
+        if char > 'z' || char < 'a' {
+            return None
+        }
+        let index = (char as usize) - ('a' as usize);
+        assert!(index < 26);
+        Some(index)
+    }
+
+    fn usize_to_char(index: usize) -> Option<char> {
+        if index >= 26 {
+            return None
+        }
+        let tmp = index + ('a' as usize);
+        assert!(tmp <= u8::MAX as usize);
+        let char = tmp as u8 as char;
+        assert!(char >= 'a' && char <= 'z');
+        Some(char)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::read_lines_untrimmed_from_file;
+    use super::*;
+
+    fn get_example_input() -> Vec<String> {
+        vec![
+            "eedadn".to_string(),
+            "drvtee".to_string(),
+            "eandsr".to_string(),
+            "raavrd".to_string(),
+            "atevrs".to_string(),
+            "tsrnev".to_string(),
+            "sdttsa".to_string(),
+            "rasrtv".to_string(),
+            "nssdts".to_string(),
+            "ntnada".to_string(),
+            "svetve".to_string(),
+            "tesnvt".to_string(),
+            "vntsnd".to_string(),
+            "vrdear".to_string(),
+            "dvrsen".to_string(),
+            "enarar".to_string(),
+        ]
+    }
+
+    #[test]
+    fn check_examples_part_1() {
+        let v = get_example_input();
+
+        assert_eq!(part_1(&v), Ok("easter".to_string()));
+    }
+
+    #[test]
+    fn check_input_part_1() -> std::io::Result<()> {
+        let input_name = "input/year_2016/input_day_06.txt";
+        let input = read_lines_untrimmed_from_file(input_name)?;
+
+        assert_eq!(part_1(&input), Ok("ygjzvzib".to_string()));
+        Ok(())
+    }
+
+    #[test]
+    fn check_examples_part_2() {
+        let v = get_example_input();
+
+        assert_eq!(part_2(&v), Ok("advent".to_string()));
+    }
+
+    #[test]
+    fn check_input_part_2() -> std::io::Result<()> {
+        let input_name = "input/year_2016/input_day_06.txt";
+        let input = read_lines_untrimmed_from_file(input_name)?;
+
+        assert_eq!(part_2(&input), Ok("pdesmnoz".to_string()));
+        Ok(())
+    }
+}
